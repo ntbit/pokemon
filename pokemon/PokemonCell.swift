45341481ec6cf1bfb9de3d72579d21782a1fd4bd
Tuan Binh <ntb.cs2@gmail.com>
2016-07-06 18:08:02 +0700

    Display image

--- pokemon/PokemonCell.swift
+++ pokemon/PokemonCell.swift
@@ -6,9 +6,25 @@
 //  Copyright © 2016 Macbook . All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class PokemonCell: UICollectionViewCell {
 
+    @IBOutlet weak var imgThumb: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
     
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon)  {
+        
+        self.pokemon = pokemon
+        
+        self.lblName.text = self.pokemon.pokeName.capitalizedString
+        self.imgThumb.image = UIImage(named: "\(self.pokemon.pokeId)")
+    }
 }
--- pokemon/ViewController.swift
+++ pokemon/ViewController.swift
@@ -8,14 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,
+    UICollectionViewDataSource,
+    UICollectionViewDelegate,
+    UICollectionViewDelegateFlowLayout{
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
     }
-
-
-
-}
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let indentifier = "pokeCell"
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier(indentifier, forIndexPath: indexPath) as? PokemonCell{
+            
+            let pokemon = Pokemon(pokeName: "POKE", pokeId: "\(indexPath.row)")
+        
+            cell.configureCell(pokemon)
+            
+            return cell
+        }else{
+            
+            return UICollectionViewCell()
+        }
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 718
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+   }
 
